0025df39eba2a2d1b255fa6f2a5105c5fb1debcf
1171060 <jack@thinkforwardtech.com>
2023-10-27 11:45:58 -0700

    Change code again to use JSX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+import logo from './assets/images/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
